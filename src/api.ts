7de34d85cbfdf9a482b67316e4d811af9db79753
Sagar Aryal <sagaryal3@gmail.com>
2024-06-30 10:07:18 +0545

    Levo corrected _redirects, changed api to fly.io

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { UserEvent, User, UpdateUserEvent, CreateUserEvent } from "./types";
 import { getLocalUser } from "./utils";
 
-const BACKEND_URL = "https://levo-calendar-frontend.fly.dev/api";
+const BACKEND_URL = "https://levo-calendar-backend.fly.dev/api";
 
 export const fetchEvents = async (): Promise<UserEvent[]> => {
   try {
